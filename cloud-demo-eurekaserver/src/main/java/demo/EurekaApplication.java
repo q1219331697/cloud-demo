f69f1ba816d8cd8ba90e466246248ca66f4902e7
tom <1219331697@qq.com>
2016-09-18 19:10:05 +0800

    更改服务发现客户端

--- cloud-demo-eurekaserver/src/main/java/demo/EurekaApplication.java
+++ cloud-demo-eurekaserver/src/main/java/demo/EurekaApplication.java
@@ -18,7 +18,7 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -27,7 +27,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class EurekaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
